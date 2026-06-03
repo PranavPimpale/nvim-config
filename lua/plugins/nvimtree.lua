3d394ee37b02f5f964b1fc0c94355811d1db1f1c
SasuKe <sasukeuchihateam14@gmail.com>
2026-06-03 16:02:22 +0530

    plugin: updated features in neovim tree

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -5,11 +5,28 @@ return {
   },
 
   config = function()
-    require("nvim-tree").setup()
+    require("nvim-tree").setup({
+      renderer = {
+        root_folder_label = function(path)
+          local parent = vim.fn.fnamemodify(path, ":h:t")
+          local current = vim.fn.fnamemodify(path, ":t")
+
+          return parent .. "/" .. current
+        end,
+      },
+    })
 
     vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", {
       noremap = true,
       silent = true,
+    })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "NvimTree",
+      callback = function()
+        vim.keymap.set("n", "j", "j^", { buffer = true, remap = true })
+        vim.keymap.set("n", "k", "k^", { buffer = true, remap = true })
+      end,
     })
   end,
 }
