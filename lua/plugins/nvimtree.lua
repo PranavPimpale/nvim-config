e20130fefe715dd4d35281fef661115add534657
SasuKe <sasukeuchihateam14@gmail.com>
2026-06-03 16:22:47 +0530

    plugin/nvim-tree : thickness func & close on opening file feature

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -6,6 +6,19 @@ return {
 
   config = function()
     require("nvim-tree").setup({
+      -- thickness for the tree
+      view = {
+        width = 26,
+      },
+
+      -- closing the window after opening a file 
+      actions = {
+        open_file = {
+          quit_on_open = true,
+        },
+      },
+
+      -- current folder on the top of the nvim tree
       renderer = {
         root_folder_label = function(path)
           local parent = vim.fn.fnamemodify(path, ":h:t")
@@ -16,11 +29,13 @@ return {
       },
     })
 
+    -- nvim tree toggle key map
     vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", {
       noremap = true,
       silent = true,
     })
 
+    -- cursor behaviour while scrolling through the tree
     vim.api.nvim_create_autocmd("FileType", {
       pattern = "NvimTree",
       callback = function()
