e2af165131a371848084c9d8148536b756502521
SasuKe <sasukeuchihateam14@gmail.com>
2026-06-09 19:42:35 +0530

    created and updated blankline in lua/plugins/blankline.lua

---
+++ lua/plugins/blankline.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    --@module "ibl"
+    --@type ibl.config
+    opts = {},
+    config = function()
+      local highlight = {
+        "DarkBlue",
+      }
+      local hooks = require "ibl.hooks"
+      -- create the highlight groups in the highlight setup hook, so they are reset
+      -- every time the colorscheme changes
+      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+        vim.api.nvim_set_hl(0, "DarkBlue", {
+          fg = "#272727",
+          bg = "NONE",
+          nocombine = true,
+        })
+      end)
+
+      require("ibl").setup {
+        indent = { highlight = highlight, char = "│",},
+        whitespace = {
+          highlight = highlight,
+          remove_blankline_trail = false,
+        },
+        scope = {
+          enabled = true,
+          -- to disable function underlines
+          show_start = false,
+          show_end = false,
+        },
+      }
+    end,
+  },
+}
