cc0bcf98390664d82fae7f1568aaf3040de5c2a8
SasuKe <sasukeuchihateam14@gmail.com>
2026-06-11 19:48:17 +0530

    init.lua : removed format on save

--- init.lua
+++ init.lua
@@ -75,15 +75,3 @@ vim.api.nvim_create_autocmd("VimEnter", {
     })
   end,
 })
-
--- auto format after save
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  callback = function()
-    local view = vim.fn.winsaveview()
-
-    vim.cmd("normal! gg=G")
-
-    vim.fn.winrestview(view)
-  end,
-})
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,23 +1,23 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    -- Use the main branch if you aren't already
-    branch = "main",
-    build = ":TSUpdate",
-    config = function()
-        local ts = require("nvim-treesitter")
+  'nvim-treesitter/nvim-treesitter',
+  -- Use the main branch if you aren't already
+  branch = "main",
+  build = ":TSUpdate",
+  config = function()
+    local ts = require("nvim-treesitter")
 
-        ts.setup({
-            ensure_installed = {
-                "lua",
-                "c",
-                "cpp",
-                "python",
-            }
-        })
-      vim.api.nvim_create_autocmd('FileType', {
-            callback = function()
-                pcall(vim.treesitter.start)
-            end,
-        })
-    end
+    ts.setup({
+      ensure_installed = {
+        "lua",
+        "c",
+        "cpp",
+        "python",
+      }
+    })
+    vim.api.nvim_create_autocmd('FileType', {
+      callback = function()
+        pcall(vim.treesitter.start)
+      end,
+    })
+  end
 }
