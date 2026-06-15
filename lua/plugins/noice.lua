e01d3ce175bc5a4197f138ebf44af797c65b6640
SasuKe <sasukeuchihateam14@gmail.com>
2026-06-15 19:42:11 +0530

    noice.lua : optimized

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -7,7 +7,19 @@ return {
   },
   opts = {
     lsp = {
+      progress = {
+        enabled = false,
+      },
+
+      hover = {
+        enabled = false,
+      },
+
       signature = {
+        enabled = false,
+      },
+
+      message = {
         enabled = false,
       },
     },
@@ -15,19 +27,23 @@ return {
     cmdline = {
       enabled = true,
       format = {
-        cmdline = {
-          icon = ":",
-        },
-        search_down = {
-          icon = "/",
-        },
-        search_up = {
-          icon = "?",
-        },
-        lua = {
-          icon = ":",
-        },
+        cmdline = { icon = ":" },
+        search_down = { icon = "/" },
+        search_up = { icon = "?" },
+        lua = { icon = ":" },
       },
+    },
+
+    popupmenu = {
+      enabled = true,
+    },
+
+    presets = {
+      bottom_search = false,
+      command_palette = false,
+      long_message_to_split = true,
+      inc_rename = false,
+      lsp_doc_border = false,
     },
 
     views = {
