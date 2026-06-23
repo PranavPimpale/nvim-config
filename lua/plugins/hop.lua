8c33d21ba3f00e1b55ef4d4ba2f40caf4f1a89db
SasuKe <sasukeuchihateam14@gmail.com>
2026-06-23 14:36:30 +0530

    new : hop.lua

---
+++ lua/plugins/hop.lua
@@ -0,0 +1,35 @@
+return {
+  "smoka7/hop.nvim",
+  version = "*",
+  opts = {},
+  keys = {
+    {
+      "f",
+      function()
+        require("hop").hint_words()
+      end,
+      desc = "Hop Words",
+    },
+    {
+      "L",
+      function()
+        require("hop").hint_lines_skip_whitespace()
+      end,
+      desc = "Hop Lines",
+    },
+    {
+      "t",
+      function()
+        require("hop").hint_char1()
+      end,
+      desc = "Hop char1",
+    },
+    {
+      "T",
+      function()
+        require("hop").hint_char2()
+      end,
+      desc = "Hop char2",
+    },
+  },
+}
